7086643bf4dbeec12a8321bf2811b4893fe90d78
Skewit <pw04ast@eduvaud.ch>
2026-01-13 14:12:44 +0100

    Feat(L2-start): Fini l'exercice
    [10] [DONE]

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const product = 'Skibidi'
+const description = "Big fluffy boots"
+const app = Vue.createApp({
+    data() {
+        return {
+            product: product,
+            description: description,
+        }
+    }
+});
