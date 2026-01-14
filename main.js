c78677aae8ffc4f1971230ed5ab8c5bc933a2b16
Skewit <pw04ast@eduvaud.ch>
2026-01-14 09:04:45 +0100

    Feat(L10-start): Fini l'exercice
    [20] [DONE]

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id) 
+        },
+        removeFromCart(id) {
+            let idIndex = this.cart.indexOf(id) 
+            if (idIndex !== -1)
+                this.cart.splice(idIndex, 1)
+        }
+    }
 })
